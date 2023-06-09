e80d044e91b3e8610af1d3494da8d77e4851c817
nate-4000 <nathanieltebo8@gmail.com>
2023-06-08 23:45:38 -0700

    added game visualizer

---
+++ creategamegraph.py
@@ -0,0 +1,20 @@
+import graphviz
+import gas
+
+game_file = gas.get("game.json")
+
+def create_graph(game_file):
+    dot = graphviz.Digraph(format='png')
+    dot.attr(rankdir='LR')
+    for index, item in enumerate(game_file):
+        dialog = item['dialog']
+        dot.node(str(index), dialog)
+    for index, item in enumerate(game_file):
+        options = item['options']
+        for option in options:
+            target_index, label = option
+            dot.edge(str(index), str(target_index), label=label)
+
+    dot.render('game_graph')
+
+create_graph(game_file)

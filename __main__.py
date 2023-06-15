06512a8a87b44f29cf62b19d38eee9536400746f
nate-4000 <nathanieltebo8@gmail.com>
2023-06-15 00:07:09 -0700

    more game in chapters

--- __main__.py
+++ __main__.py
@@ -9,8 +9,8 @@ def displayDialog(dialog):
     choice = input("> ")
     return int(choice) if choice.isdigit() else choice
 
-def runGame():
-    gameData = gas.get("game.json")
+def runGame(game):
+    gameData = gas.get("chpt%s.json" % game)
     
     currentDialogIndex = 0
     while True:
@@ -20,8 +20,18 @@ def runGame():
         if isinstance(choice, int) and 0 <= choice < len(currentDialog["options"]):
             nextDialogIndex = currentDialog["options"][choice][0]
             currentDialogIndex = nextDialogIndex
+            if currentDialogIndex == "explosion":
+                return
         else:
             print("Invalid option. Please try again.")
 
 if __name__ == "__main__":
-    runGame()
+    print("welcome to td32")
+    while True:
+        print("please select a chapter:")
+        print("""
+    1. the escape
+    2. infiltration
+        """)
+        the = input("> ")
+        runGame(the)
